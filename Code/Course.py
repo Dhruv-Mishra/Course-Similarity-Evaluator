7f57462403e4e06d1b5103ed8d753a66466c0cc9
Dhruv Mishra <88545875+Dhruv-Mishra@users.noreply.github.com>
2023-05-11 05:15:29 +0530

    Update Course.py

--- Code/Course.py
+++ Code/Course.py
@@ -16,6 +16,7 @@ warnings.simplefilter(action='ignore', category=UserWarning)
 embed = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")
 
 class Course:
+
     course_name_list = set([])
     file_name = ""
     code = ""
@@ -25,25 +26,36 @@ class Course:
     file_ptr = ""
     name = ""
 
+    def pre_process_text(self, new_s):
+        new_s = ".".join(new_s.split("\n"))
+        #new_s = " ".join(new_s.split("-"))
+        new_s = new_s.lower()
+        # translate_table = dict((ord(char), " ") for char in string.punctuation)   
+        # new_s = new_s.translate(translate_table)
+        # li = word_tokenize(new_s)
+        # stop_words = set(stopwords.words("english"))
+        # filter_li = []
+        # for words in li:
+        #     if(words not in stop_words):
+        #         filter_li.append(words)
+        # ans = " ".join(filter_li)
+        ans = new_s
+        return ans
+
     def __init__(self, file_pointer):
-        self.file_name = str(file_pointer)
+        self.file_name = str(f)
         self.file_ptr = file_pointer
-        self.description = self.find_description()
+        self.description = self.find_description() + " " + self.find_course_plan()
         self.code = self.find_code()
         self.name = self.find_name()
-        if(self.description != -1):
-            self.embedding = self.compute_embedding()
+
 
     def find_description(self):
         workbook = openpyxl.load_workbook(self.file_ptr)
         worksheet = workbook.active
-        description_row = -1
-        description_col = -1
         for coll in range(1,6):
             for roww in range(1,20):
                 if("description" in "".join(re.sub(r'[^\w\s]','', re.sub(r'\d+','',str(worksheet.cell(row=roww, column=coll).value).lower())).split())):
-                    description_row = roww
-                    description_col = coll
                     return str(worksheet.cell(row=roww, column=coll+1).value)
         return "-1"
     
@@ -84,6 +96,32 @@ class Course:
             file_name = file_name[-2]
         file_name = "".join(file_name.lower().split())
         return file_name
-
+    
+    def find_course_plan(self):
+        f = self.file_ptr
+        workbook = openpyxl.load_workbook(f)
+        worksheet = workbook.active
+        lec_cell = (-1,-1)
+        for coll in range(1,20):
+            for roww in range(1,41):
+                cell_val = "".join("".join(str(worksheet.cell(row=roww, column=coll).value).lower().strip().split()).split("-"))
+                if(len(cell_val) >= 2 and len(cell_val) <= 30 and 'lecture' in cell_val and 'topic' in cell_val):
+                    lec_cell = (roww,coll)
+                    break
+            if(lec_cell[0]!=-1):
+                break
+        if(lec_cell[0] == -1):
+            return "-1"
+        plan = ""
+        for i in range(8):
+            new_row = lec_cell[0]+i+1
+            new_col = lec_cell[1]
+            cell_val = str(worksheet.cell(row=new_row, column=new_col).value).lower().strip()
+            plan = plan + ". " + cell_val
+        plan = self.pre_process_text(plan)
+        plan = ". ".join(plan.split("•"))
+        plan = plan + "."
+        return plan
+    
     def compute_embedding(self):
         return embed([self.description])
